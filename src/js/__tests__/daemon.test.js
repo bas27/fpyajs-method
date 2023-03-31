c4ff11ab6a9075cb9698d0cf82f9b61d39b8d3c5
Alexander B <bas2784@gmail.com>
2023-03-31 11:56:33 +0300

    lint fix

--- src/js/__tests__/daemon.test.js
+++ src/js/__tests__/daemon.test.js
@@ -8,10 +8,9 @@ const res = {
   attack: 10,
   defence: 40,
 };
-describe("tests daemon character", () => {
+describe('tests daemon character', () => {
   it('Test class', () => {
     const d1 = new Daemon('dai');
     expect(d1).toEqual(res);
   });
-
 });
